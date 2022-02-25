135e3b7b20587fbc01d2d0462abdb2b2f69e8048
Samuel Wallace <swallace17@users.noreply.github.com>
2022-02-25 16:47:25 -0500

    Add comments, Mac directory

--- Windows/native_vpn_disconnect.bat
+++ Windows/native_vpn_disconnect.bat
@@ -1,1 +1,5 @@
+:: Disconnect current Windows-native VPN session
 rasdial /d
+
+:: Per MS Docs ( https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-r2-and-2012/ff859533(v=ws.11) ):
+  ::/disconnect - Required when disconnecting. Disconnects the specified entry. You can also disconnect by typing /d.
