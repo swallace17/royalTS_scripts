8ae9b6d25edcfc6047dfe6c136c054ebcef834d7
Samuel Wallace <swallace17@users.noreply.github.com>
2022-03-01 18:46:20 -0500

    Updated comments in Windows scripts

--- Windows/native_vpn_connect.bat
+++ Windows/native_vpn_connect.bat
@@ -1,5 +1,5 @@
-::
-rasdial %1 %2 %3
-
+::Use rasdial command to connect to existing Windows built-in VPN
 :: Per MS Docs ( https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-r2-and-2012/ff859533(v=ws.11) ):
-  ::/disconnect - Required when disconnecting. Disconnects the specified entry. You can also disconnect by typing /d.
+
+:: Arguments: VPN Name, Username, Password
+rasdial %1 %2 %3
